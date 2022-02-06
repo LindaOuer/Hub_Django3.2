480186c66f3aed5511c1f02eafd3936f899b81a1
LindaOuer <linda.ouerfelli@gmail.com>
2022-02-06 21:43:55 +0100

    Models OneToOne Field

--- Hub/models.py
+++ Hub/models.py
@@ -27,3 +27,9 @@ class Project(models.Model):
     description = models.TextField(max_length=250)
 
     isValid = models.BooleanField(default=False)
+    
+    creator = models.OneToOneField(
+        to = Student, 
+        on_delete = models.CASCADE, 
+        related_name = "project_owner"
+    )
