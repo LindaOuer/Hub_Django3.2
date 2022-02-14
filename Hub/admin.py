e51b2b2aa77298ef227a1d270434ff53cc760ded
LindaOuer <linda.ouerfelli@gmail.com>
2022-02-14 08:14:06 +0100

    Admin: Custom Display - Student

--- Hub/admin.py
+++ Hub/admin.py
@@ -2,4 +2,22 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register([Project, Student, Coach])
+#admin.site.register([Project, Student, Coach])
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = (
+        'last_name',
+        'first_name',
+        'email'
+    )
+    fields = (
+        ('last_name', 'first_name'),
+        'email'
+    )
+
+
+admin.site.register(Student, StudentAdmin)
+
+admin.site.register(Coach)
+
+admin.site.register(Project)
