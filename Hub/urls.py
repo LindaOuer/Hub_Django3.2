86355ca016d84eea4acb7cf35fd15c234e966987
LindaOuer <linda.ouerfelli@gmail.com>
2022-02-27 19:24:48 +0100

    Views: Function Based - list Students

--- Hub/urls.py
+++ Hub/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import homePage
+from .views import homePage, list_Students
 
 urlpatterns = [
     path('', homePage, name="Home_Page"),
+    path('listStudents', list_Students, name='Student_list'),
 ]
--- Hub/views.py
+++ Hub/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
+from .models import Student
+
 # Create your views here.
 # function based views
+
+
 def homePage(request):
     return HttpResponse('<h1>Welcome To ... </h1>')
+
+
+def list_Students(request):
+    list = Student.objects.all()
+    return render(
+        request,
+        'Hub/list_students.html',
+        {
+            'students': list,
+        },
+    )
