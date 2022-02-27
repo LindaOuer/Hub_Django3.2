342b5bd1f2334e214b1140d16886a565ada7709c
LindaOuer <linda.ouerfelli@gmail.com>
2022-02-27 18:57:31 +0100

    Admin: Project Admin - Custom Filter

--- Hub/admin.py
+++ Hub/admin.py
@@ -40,6 +40,22 @@ class CoachAdmin(admin.ModelAdmin):
     )
     search_fields = ['last_name']
 
+class ProjetDurationListFilter(admin.SimpleListFilter):
+    title = 'Duration'
+    parameter_name = 'duration'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('1 month', ("less than a month")),
+            ('3 months', ("More than 3 months"))
+        )
+
+    def queryset(self, request, queryset):
+        if self.value() == '1 month':
+            return queryset.filter(project_duration__lte=30)
+        if self.value() == '3 months':
+            return queryset.filter(project_duration__lte=90, project_duration__gte=30)
+
 class ProjectAdmin(admin.ModelAdmin):
     list_display = (
         'project_name',
@@ -51,6 +67,7 @@ class ProjectAdmin(admin.ModelAdmin):
     list_filter = (
         'creator',
         'isValid',
+        ProjetDurationListFilter,
     )
     
     #date_hierarchy = 'updated_at'
