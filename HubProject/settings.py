699ff243fa4b3fad6064c291e6c0e5b6a36a1c3b
LindaOuer <linda.ouerfelli@gmail.com>
2022-02-27 21:24:20 +0100

    Templates: Best Practice

--- HubProject/settings.py
+++ HubProject/settings.py
@@ -59,7 +59,9 @@ ROOT_URLCONF = 'HubProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'HubProject/templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
